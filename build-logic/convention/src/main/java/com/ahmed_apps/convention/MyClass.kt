9776d70ff8c7363dd1c5ad9a5ededc9205ea7dc4
ahmed-guedmioui <ahmedgire01@gmail.com>
2024-05-23 19:41:08 +0100

    Setting up the build-logic module

---
+++ build-logic/convention/src/main/java/com/ahmed_apps/convention/MyClass.kt
@@ -0,0 +1,4 @@
+package com.ahmed_apps.convention
+
+class MyClass {
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,5 @@
 pluginManagement {
+    includeBuild("build-logic")
     repositories {
         google {
             content {
@@ -19,8 +20,8 @@ dependencyResolutionManagement {
     }
 }
 
-enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 rootProject.name = "RunningTrackerApp"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 include(":app")
 include(":auth:data")
 include(":auth:domain")
