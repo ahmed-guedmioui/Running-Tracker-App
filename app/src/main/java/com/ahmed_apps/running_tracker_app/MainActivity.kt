d35202bbaa56c5a68761ec5b574ee00148aef8fb
ahmed-guedmioui <ahmedgire01@gmail.com>
2024-05-24 13:04:56 +0100

    Creating the Material Theme

--- app/src/main/java/com/ahmed_apps/running_tracker_app/MainActivity.kt
+++ app/src/main/java/com/ahmed_apps/running_tracker_app/MainActivity.kt
@@ -4,44 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.ahmed_apps.running_tracker_app.ui.theme.RunningTrackerAppTheme
+import com.ahmed_apps.core.presentation.designsystem.RunningTrackerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            RunningTrackerAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            RunningTrackerTheme {
+
+
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RunningTrackerAppTheme {
-        Greeting("Android")
-    }
-}
