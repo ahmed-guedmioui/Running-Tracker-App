97329939f989c20b69dff31fd3ec877b4e2d32a9
ahmed-guedmioui <ahmedgire01@gmail.com>
2024-05-25 12:06:49 +0100

    Validating user data

--- auth/presentation/src/main/java/com/ahmed_apps/auth/presentation/register/RegisterViewModel.kt
+++ auth/presentation/src/main/java/com/ahmed_apps/auth/presentation/register/RegisterViewModel.kt
@@ -1,16 +1,43 @@
 package com.ahmed_apps.auth.presentation.register
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.text2.input.textAsFlow
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ahmed_apps.auth.domain.UserDataValidator
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 /**
  * @author Ahmed Guedmioui
  */
-class RegisterViewModel : ViewModel() {
+@OptIn(ExperimentalFoundationApi::class)
+class RegisterViewModel(
+    private val userDataValidator: UserDataValidator
+) : ViewModel() {
     var state by mutableStateOf(RegisterState())
         private set
+
+    init {
+        state.email.textAsFlow()
+            .onEach { email ->
+                state = state.copy(
+                    isEmailValid = userDataValidator.isValidEmail(email.toString())
+                )
+            }
+            .launchIn(viewModelScope)
+
+        state.password.textAsFlow()
+            .onEach { password ->
+                state = state.copy(
+                    passwordValidationState = userDataValidator.isValidPassword(password.toString())
+                )
+            }
+            .launchIn(viewModelScope)
+    }
 
     fun onAction(action: RegisterAction) {
         when (action) {
