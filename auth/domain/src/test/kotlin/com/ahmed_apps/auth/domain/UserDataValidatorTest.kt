31e76cde5f497a4e616157b6a80594aa17478cc4
ahmed-guedmioui <ahmedgire01@gmail.com>
2024-08-28 13:55:02 +0100

    Testing UserDataValidator

--- auth/domain/src/test/kotlin/com/ahmed_apps/auth/domain/UserDataValidatorTest.kt
+++ auth/domain/src/test/kotlin/com/ahmed_apps/auth/domain/UserDataValidatorTest.kt
@@ -2,15 +2,41 @@ package com.ahmed_apps.auth.domain
 
 import assertk.assertThat
 import assertk.assertions.isEqualTo
-import org.junit.jupiter.api.Assertions.*
+import assertk.assertions.isTrue
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 /**
  * @author Ahmed Guedmioui
  */
 class UserDataValidatorTest {
-    @Test
-    fun testAddingTwoNumbers() {
-        assertThat(1 + 2).isEqualTo(3)
+
+    private lateinit var userDataValidator: UserDataValidator
+
+    @BeforeEach
+    fun setUp() {
+        userDataValidator = UserDataValidator(
+            patternValidator = object : PatternValidator {
+                override fun matches(value: String): Boolean {
+                    return true
+                }
+            }
+        )
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "Password123, true",
+        "password123, false",
+        "354523, false",
+        "Passwod1-23, true",
+        "TT4646GGFH, false",
+    )
+    fun testIsValidPassword(password: String, expected: Boolean) {
+        val state = userDataValidator.isValidPassword(password)
+
+        assertThat(state.isValidPassword).isEqualTo(expected)
     }
 }
